ace01e8788024f646b18608cc842bc76aa5f7cc6
Spektoruk3 <ispektorukace@gmail.com>
2023-10-13 14:13:56 +0700

    add

--- i.spektoruk/ex_time.c
+++ i.spektoruk/ex_time.c
@@ -31,7 +31,7 @@ int main()
     else {
         printf("%d/%d/%02d %d:%02d %s\n",
             sp->tm_mon + 1, sp->tm_mday,
-            sp->tm_year, sp->tm_hour,
+            sp->tm_year - 100, sp->tm_hour,
             sp->tm_min, tzname[sp->tm_isdst]);
     }
     
